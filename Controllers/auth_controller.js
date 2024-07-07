468c4eb1c554279ff2670919f9f486e1825a3862
iftekharalammithu45 <iftekharalammithu45@gmail.com>
2024-07-07 15:36:15 +0000

    Sign up set up

--- Controllers/auth_controller.js
+++ Controllers/auth_controller.js
@@ -1,10 +1,44 @@
+import User from "../Models/User_Model.js";
+import bcryptjs from "bcryptjs";
+
 const usersignup = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
-    res.send("signup successfull");
-    console.log(name, email, password);
+    const { name, username, password, confirmPassword, gender } = req.body;
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "password not match" });
+    }
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({ message: "user already exist" });
+    }
+
+    const boyprofilepic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlprofilepic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      name,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyprofilepic : girlprofilepic,
+    });
+
+    if (newUser) {
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        name: newUser.name,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ message: "invalid user data" });
+    }
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
     res.send("signup failed");
   }
 };
