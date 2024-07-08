ad7ac41384211348fcbe3fc2574d394fcefbebc5
iftekharalammithu45 <iftekharalammithu45@gmail.com>
2024-07-08 15:00:01 +0000

    Message update

--- Controllers/Message_controller.js
+++ Controllers/Message_controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../Models/Conversation_Model.js";
 import Message from "../Models/Message_Model.js";
+import User from "../Models/User_Model.js";
 
 const sendMessage = async (req, res) => {
   try {
@@ -24,8 +25,11 @@ const sendMessage = async (req, res) => {
     if (newmessage) {
       conversation.messages.push(newmessage._id);
     }
-    await conversation.save();
-    await newmessage.save();
+    // await conversation.save();
+    // await newmessage.save();
+
+    // Save both the new message and the updated conversation concurrently
+    await Promise.all([conversation.save(), newmessage.save()]);
 
     res.status(200).json(newmessage);
   } catch (err) {
@@ -34,4 +38,29 @@ const sendMessage = async (req, res) => {
   }
 };
 
-export { sendMessage };
+const getmessage = async (req, res) => {
+  try {
+    const userToChatId = req.params.receiverid;
+    const senderid = req.user;
+
+    const userToChat = await User.findById(userToChatId);
+    if (!userToChat) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Find a conversation that includes both the sender and the recipient
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderid, userToChatId] }, // Match conversations with both participants
+    }).populate("messages"); // Populate the messages field with the actual message data
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+
+    return res.status(200).json(conversation.messages);
+  } catch (error) {
+    console.log("Error to get Message");
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+export { sendMessage, getmessage };
--- Route/Message_route.js
+++ Route/Message_route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { sendMessage } from "../Controllers/Message_controller.js";
+import { getmessage, sendMessage } from "../Controllers/Message_controller.js";
 import { protectroute } from "../Middle_ware/Message_Middle_ware.js";
 
 const messageroute = express.Router();
 
 messageroute.post("/message/:receiverid", protectroute, sendMessage);
+messageroute.get("/:receiverid", protectroute, getmessage);
 
 export default messageroute;
