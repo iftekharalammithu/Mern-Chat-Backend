6579815fd507207a4aa105c19dbedfe94112b263
iftekharalammithu45 <iftekharalammithu45@gmail.com>
2024-07-11 15:19:17 +0000

    update

--- Utils/generateToken.js
+++ Utils/generateToken.js
@@ -7,11 +7,10 @@ const generateToken = (userid, res) => {
     expiresIn: "30d",
   });
   res.cookie("jwt", token, {
-    maxAge: 24 * 60 * 60 * 1000, // Cookie expires in 30 days
+    maxAge: 30 * 24 * 60 * 60 * 1000, // Cookie expires in 30 days
     httpOnly: true, // Prevent client-side access to the cookie
-    // Suggested code may be subject to a license. Learn more: ~LicenseLog:3656161525.
-    sameSite: "Lax", // Prevent CSRF attacks
-    secure: false,
+    sameSite: "Strict", // Prevent CSRF attacks
+    secure: true,
   });
 };
 
