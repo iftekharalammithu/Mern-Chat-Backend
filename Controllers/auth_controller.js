73917581fe7a74ef41e3191529b4ff10527a4d63
iftekharalammithu45 <iftekharalammithu45@gmail.com>
2024-07-11 12:19:35 +0000

    bug fixed

--- Controllers/auth_controller.js
+++ Controllers/auth_controller.js
@@ -70,7 +70,7 @@ const userlogin = async (req, res) => {
 
 const userlogout = async (req, res) => {
   try {
-    res.cookie("jwt", "", {
+    res.cookie(process.env.JWS_NAME, "", {
       httpOnly: true,
       expires: new Date(Date.now()),
     });
