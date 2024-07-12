b94989334b7c2fda663864f43dcd918acc7def3b
iftekharalammithu45 <iftekharalammithu45@gmail.com>
2024-07-12 15:52:34 +0000

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 app.set("trust proxy", 1);
 const corsOptions = {
-  origin: "https://testchat-r4a4y1tts-mithus-projects-9636a7cf.vercel.app",
+  origin: "https://previous-kittens.surge.sh",
   credentials: true,
 };
 
