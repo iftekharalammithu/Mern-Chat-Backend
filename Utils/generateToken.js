fb0fa6db5f96edbb4b6af110e7c8e9184451ea7d
iftekharalammithu45 <iftekharalammithu45@gmail.com>
2024-07-12 15:09:49 +0000

    update

--- Utils/generateToken.js
+++ Utils/generateToken.js
@@ -11,6 +11,8 @@ const generateToken = (userid, res) => {
     httpOnly: true, // Prevent client-side access to the cookie
     sameSite: "Lax", // Prevent CSRF attacks
     secure: true,
+    domain: "mern-chat-backend-3af8.onrender.com",
+    path: '/', 
   });
 };
 
