1a54fe4ad8e58cf3acc84cd3c393216883f0706f
iftekharalammithu45 <iftekharalammithu45@gmail.com>
2024-07-14 05:34:42 +0000

    update

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 const corsOptions = {
   origin:
     "https://9000-idx-chat-app-mern-stack-1720103279964.cluster-7ubberrabzh4qqy2g4z7wgxuw2.cloudworkstations.dev",
+
   credentials: true,
 };
 
