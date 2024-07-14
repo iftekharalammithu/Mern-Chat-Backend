289a73a769b92cfef4342d482e64de296c3aa005
iftekharalammithu45 <iftekharalammithu45@gmail.com>
2024-07-14 05:15:40 +0000

    socket ipdate

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ dotenv.config();
 
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 // app.set("trust proxy", 1);
-const corsOptions = {
-  origin:
-    "https://9000-idx-chat-app-mern-stack-1720103279964.cluster-7ubberrabzh4qqy2g4z7wgxuw2.cloudworkstations.dev",
-  credentials: true,
-};
+// const corsOptions = {
+//   origin:
+//     "https://9000-idx-chat-app-mern-stack-1720103279964.cluster-7ubberrabzh4qqy2g4z7wgxuw2.cloudworkstations.dev",
+//   credentials: true,
+// };
 
 // app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // set the router
 app.use("/api/auth/", router);
